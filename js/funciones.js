c3160b2dea6f26acabed62c9ba9b2b63493068e6
DiegoDavid17 <diegodavidalmiron@gmail.com>
2024-01-08 21:25:13 -0300

    primer commit

---
+++ js/funciones.js
@@ -0,0 +1,307 @@
+function verPortada(){
+    Swal.fire({
+        title: "Portada",
+        text: "Puente General Belgrano (Corrientes, Argentina)",
+        imageUrl: "images/header.jfif",
+        width: 4000,
+        imageWidth: 4000,
+        confirmButtonColor: "rgb(34, 3, 70)"
+        
+        });
+}
+
+function verPerfil(){
+    Swal.fire({
+        title: "Diego David Almirón",
+        text: "",
+        imageUrl: "images/perfil.png",
+        width: 400,
+        imageWidth: 400,
+        confirmButtonColor: "rgb(34, 3, 70)"
+        });
+}
+
+function masDatos(){
+    Swal.fire({
+        icon: 'question',
+        iconHtml: '<i class="bi bi-person-vcard" style="color: rgb(34, 3, 70);"></i>',
+        html:`<div class="card border-dark border-2" style="font-family:'Anton';">
+                <div class="card-header h4 bg-secondary text-white border-dark border-2">Más Datos</div>
+                <div class="card-body">
+                    <p class="bi-calendar"> Nacimiento:<span class="text-secondary"> 17-Marzo-1990</span></p>
+                    <p class="bi-calendar-heart"> Edad:<span class="text-secondary"> 33 Años</span></p>
+                    <p class="bi-gender-male"> Género:<span class="text-secondary"> Masculino</span></p>
+                    <p class="bi-heart"> Estado Civil:<span class="text-secondary"> Soltero</span></p>
+                    <hr class="border-2 border-dark" style="opacity: 1;">
+                    <p class="h4">Aficiones</p>
+                    <button class="btn btn-outline-dark m-2">📖 Leer</button>
+                    <button class="btn btn-outline-dark m-2">🏃 Deporte</button>
+                    <button class="btn btn-outline-dark m-2">🧉 Mate</button>
+                    <button class="btn btn-outline-dark m-2">👨‍🎓 Estudiar</button>
+                    <button class="btn btn-outline-dark m-2">🚌 Viajar</button>
+                </div>
+              </div>`,
+        confirmButtonColor: "rgb(34, 3, 70)"
+    });
+}
+
+/* Funciones de diplomas */
+
+function yoProgramo() {
+    Swal.fire({
+    title: "#yoProgramo",
+    imageUrl: "images/certificados/Certificado_YoProgramo/Certificado_YoProgramo.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+  
+
+  function seProgramar() {
+    Swal.fire({
+    title: "#seProgramar",
+    imageUrl: "images/certificados/argentina_programa_octubre_diciembre_2021/argentina_programa_octubre_diciembre_2021-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function fullStackJava() {
+    Swal.fire({
+    title: " Desarrollo Fullstack con Java",
+    imageUrl: "images/certificados/CertificadoDigitalers2021/CertificadoDigitalers2021-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function DesarrolloWebUTN() {
+    Swal.fire({
+    title: "Desarrollo Web HTML5 y CSS3",
+    imageUrl: "images/certificados/CertificadoUTN/CertificadoUTN-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function bd() {
+    Swal.fire({
+    title: "Base de Datos",
+    imageUrl: "images/certificados/CertificacionBaseDeDatos/CertificacionBaseDeDatos-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function hibernate() {
+    Swal.fire({
+    title: "Java Hibernate",
+    imageUrl: "images/certificados/certificado Java Hibernate/certificado Java Hibernate-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function spring() {
+    Swal.fire({
+    title: "Java Spring",
+    imageUrl: "images/certificados/Java Spring certificado/Java Spring certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function j2se() {
+    Swal.fire({
+    title: "Java Standard Web Programing",
+    imageUrl: "images/certificados/Java Standard Web Programming, J2SE Certificado/Java Standard Web Programming, J2SE Certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function javascript() {
+    Swal.fire({
+    title: "JavaScript",
+    imageUrl: "images/certificados/Javascript desde cero certificado/Javascript desde cero certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function noProgramadores() {
+    Swal.fire({
+    title: "Java Para No Programadores",
+    imageUrl: "images/certificados/Java para no programadores certificado/Java para no programadores certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function react() {
+    Swal.fire({
+    title: "React JS",
+    imageUrl: "images/certificados/ReactJS Certificado/ReactJS Certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function git() {
+    Swal.fire({
+    title: "Git",
+    imageUrl: "images/certificados/certificadoGit/certificadoGit-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function seguridadInformatica() {
+    Swal.fire({
+    title: "Seguridad Informática",
+    imageUrl: "images/certificados/Diego-David Almirón-Seguridad Informática/Diego-David Almirón-Seguridad Informática-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function oracle() {
+    Swal.fire({
+    title: "Oracle",
+    imageUrl: "images/certificados/Diego David Almirón-Oracle/Diego David Almirón-Oracle-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function fundamentosWeb() {
+    Swal.fire({
+    title: "Fundamentos Web",
+    imageUrl: "images/certificados/Fundamentos Web certificado/Fundamentos Web certificado-1.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  function corrienTech() {
+    Swal.fire({
+    title: "Corrien Tech",
+    imageUrl: "images/certificados/CorrienTech/CorrienTech.png",
+    width: 800,
+    imageWidth: 800,
+    confirmButtonColor: "rgb(34, 3, 70)"
+    
+    });
+  }
+
+  /* Funciones de institutos */
+
+
+  function educacionIt() {
+                        
+    Swal.fire({
+    imageUrl: "images/instituciones/educacion-it.jpg",
+    width: 1000,
+    imageWidth: 300,
+    icon: 'question',
+    iconHtml: '<i class="bi bi-award-fill" style="color: rgb(34, 3, 70);"></i>',
+    html:`<div class="card border-dark border-2" style="font-family:'Anton';">
+              <div class="card-header h4 bg-secondary text-white border-dark border-2">Educación IT</div>
+              <div class="card-body">
+                <p class="bi bi-award-fill"> Título:<span class="text-secondary"> Desarrollador Fullstack Java</span></p>
+                <p class="bi-calendar"> Finalización:<span class="text-secondary"> Nov - 2021</span></p>
+                <img class="img-fluid" src="images/certificados/CertificadoDigitalers2021/CertificadoDigitalers2021-1.png" alt="">
+              </div>
+            </div>`,
+    confirmButtonColor: "rgb(34, 3, 70)",
+    showCloseButton: true
+      });
+  }
+  
+  function utnBa() {
+    Swal.fire({
+    imageUrl: "images/instituciones/logo-utn.ba.jpg",
+    width: 1000,
+    imageWidth: 300,
+    icon: 'question',
+    iconHtml: '<i class="bi bi-award-fill" style="color: rgb(34, 3, 70);"></i>',
+    html:`<div class="card border-dark border-2" style="font-family:'Anton';">
+              <div class="card-header h4 bg-secondary text-white border-dark border-2">Centro de e-Learning UTN BA</div>
+              <div class="card-body">
+                <p class="bi bi-award-fill"> Título:<span class="text-secondary"> Desarrollador Web HTML5 CSS3</span></p>
+                <p class="bi-calendar"> Finalización:<span class="text-secondary"> Oct - 2017</span></p>
+                <img class="img-fluid" src="images/certificados/CertificadoUTN/CertificadoUTN-1.png" alt="">
+              </div>
+            </div>`,
+    confirmButtonColor: "rgb(34, 3, 70)",
+    showCloseButton: true
+      });
+  }
+  function argPrograma() {
+    Swal.fire({
+    imageUrl: "images/instituciones/arg-programa.png",
+    width: 1000,
+    imageWidth: 300,
+    icon: 'question',
+    iconHtml: '<i class="bi bi-award-fill" style="color: rgb(34, 3, 70);"></i>',
+    html:`<div class="card border-dark border-2" style="font-family:'Anton';">
+              <div class="card-header h4 bg-secondary text-white border-dark border-2">Argentina Programa</div>
+              <div class="card-body">
+                <p class="bi bi-award-fill"> Título:<span class="text-secondary"> Desarrollador Fullstack Junior</span></p>
+                <p class="bi-calendar"> Finalización:<span class="text-secondary"> Dic - 2022</span></p>
+                <img class="img-fluid" src="images/certificados/Certificado_YoProgramo/Certificado_YoProgramo.png" alt="">
+              </div>
+            </div>`,
+    confirmButtonColor: "rgb(34, 3, 70)",
+    showCloseButton: true
+      });
+  }
+  function facenaUnne() {
+    Swal.fire({
+    imageUrl: "images/instituciones/facena.jpg",
+    width: 1000,
+    imageWidth: 300,
+    icon: 'question',
+    iconHtml: '<i class="bi bi-award-fill" style="color: rgb(34, 3, 70);"></i>',
+    html:`<div class="card border-dark border-2" style="font-family:'Anton';">
+              <div class="card-header h4 bg-secondary text-white border-dark border-2">Facultad De Ciencias Exactas y Naturales y Agrimensura (UNNE)</div>
+              <div class="card-body">
+                <p class="bi bi-award-fill"> Título:<span class="text-secondary"> Analista Programador Universitario</span></p>
+                <p class="bi-calendar"> Finalización:<span class="text-secondary"> En curso (98% alcanzado)</span></p>
+                <img class="img-fluid" src="images/instituciones/historia_academica-1.png" alt="">
+              </div>
+            </div>`,
+    confirmButtonColor: "rgb(34, 3, 70)",
+    showCloseButton: true
+      });
+  }
